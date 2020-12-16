cff63244c82204cc20ec8b8fe8ccaf64d315dcd7
hendrixjoseph <hendixjoseph@aol.com>
2020-12-16 15:40:12 -0500

    add facebook share

--- index.js
+++ index.js
@@ -11,29 +11,50 @@ let tweet = content => {
   const access_token_key = core.getInput('twitter_access_token')
   const access_token_secret = core.getInput('twitter_access_token_secret')
 
-  core.setSecret(consumer_key);
-  core.setSecret(consumer_secret);
-  core.setSecret(access_token_key);
-  core.setSecret(access_token_secret);
-    
+  core.setSecret(consumer_key)
+  core.setSecret(consumer_secret)
+  core.setSecret(access_token_key)
+  core.setSecret(access_token_secret)
+
   let Twitter = require('twitter')
-  
+
   let client = new Twitter({
     consumer_key: consumer_key,
     consumer_secret: consumer_secret,
     access_token_key: access_token_key,
     access_token_secret: access_token_secret
   })
-  
+
   client.post('statuses/update', {status: content})
     .then(tweet => {
-        
+
         core.info(`Tweet available at https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
         core.info(`Full response:`)
         core.info(JSON.stringify(tweet))
     }).catch(error => {
         core.info(JSON.stringify(error))
     })
+}
+
+const share = (message, link) => {
+  const FB = require('fb');
+
+  const access_token = core.getInput('facebook_access_token')
+  core.setSecret(access_token)
+
+  FB.setAccessToken(access_token);
+  FB.api('/me/feed',
+         'POST',
+        { 'message': message, 'link': link},
+        response => {
+            if (response.error) {
+                core.info('error occurred')
+            } else {
+                core.info('successfully posted to page!')
+            }
+            core.info(JSON.stringify(response))
+        }
+        )
 }
 
 const source = core.getInput('source')
@@ -43,7 +64,8 @@ fetch(source)
     .then(text => parse(text, {columns: true, escape: '\\'}, (err, posts) => {
         let index = Math.floor(Math.random() * posts.length)
         let post = posts[index]
-        let content = `Classic post from ${post.date}: ${post.title} ${post.url}`
-        
-        tweet(content)
+        let content = `Classic post from ${post.date}: ${post.title}`
+
+        tweet(content + ' ' + post.url)
+        share(content, post.url)
     }))
